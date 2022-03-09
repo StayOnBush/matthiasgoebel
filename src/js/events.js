ec2fd03a220af2739fdf54628ff730ec08168cb7
StayOnBush <j-heye@web.de>
2022-03-09 18:06:21 +0100

    jan

--- src/js/events.js
+++ src/js/events.js
@@ -32,6 +32,7 @@ events_items.forEach(function(item, index) {
 let events_events_length =  Object.entries(events.events).length;
 let event_content = document.querySelector('.calendar-full-main-table-content');
 
+let event_row
 let event_date;
 let event_time;
 let event_title;
@@ -60,65 +61,128 @@ modal_action_del.innerHTML = 'Löschen';
 let modal_action_cancel = document.createElement('button');
 modal_action_cancel.classList.add('action-cancel');
 modal_action_cancel.innerHTML = 'Abbrechen';
+let i = 0;
 
-for (let i=0;i<events_events_length;i++) {
-
-    // create table row
-    let event_row = document.createElement('div');
-    event_row.classList.add('calendar-full-main-table-row');
-
-    // create column for date
-    event_date = document.createElement('div');
-    event_date.classList.add('calendar-full-main-table-date'); 
-    event_date.innerHTML = events.events[i].date;
-
-    // create column for time
-    event_time = document.createElement('div');
-    event_time.classList.add('calendar-full-main-table-time'); 
-    event_time.innerHTML = events.events[i].time;
-
-    // create column for title
-    event_title = document.createElement('div');
-    event_title.classList.add('calendar-full-main-table-title'); 
-    event_title.innerHTML = events.events[i].title;
-
-    // create column for description
-    event_desc = document.createElement('div');
-    event_desc.classList.add('calendar-full-main-table-description'); 
-    event_desc.innerHTML = events.events[i].description;
-
-    // create column for action buttons
-    let event_edit = document.createElement('button');
-    event_edit.classList.add('action_edit');
-    event_edit.innerHTML = `Bearbeiten`;
-    let event_del = document.createElement('button');
-    event_del.classList.add('action_del');
-    event_del.innerHTML = `Löschen`;
-    event_buttons = document.createElement('div');
-    event_buttons.classList.add('calendar-full-main-table-actions'); 
-    event_buttons.appendChild(event_edit);
-    event_buttons.appendChild(event_del);
-
-    event_row.appendChild(event_date);
-    event_row.appendChild(event_time);
-    event_row.appendChild(event_title);
-    event_row.appendChild(event_desc);
-    event_row.appendChild(event_buttons);
-    event_content.appendChild(event_row);
-
-    event_buttons.addEventListener('click', function(item) {
-        if(item.target == event_edit) {
-            let headline = 'Event Bearbeiten';
-            //removeModal();
-            showModal(headline, i, 1);
-        } else if(item.target == event_del) {
-            let headline = 'Event Löschen';
-            //removeModal();
-            showModal(headline, i, 2);
-        } else {
-            console.log("Fehler: Dieser Button existiert nicht.");
-        }
-    });
+function down() {
+    for (i=0;i<events_events_length;i++) {
+        // create table row
+        event_row = document.createElement('div');
+        event_row.classList.add('calendar-full-main-table-row');
+    
+        // create column for date
+        event_date = document.createElement('div');
+        event_date.classList.add('calendar-full-main-table-date'); 
+        event_date.innerHTML = events.events[i].date;
+    
+        // create column for time
+        event_time = document.createElement('div');
+        event_time.classList.add('calendar-full-main-table-time'); 
+        event_time.innerHTML = events.events[i].time;
+    
+        // create column for title
+        event_title = document.createElement('div');
+        event_title.classList.add('calendar-full-main-table-title'); 
+        event_title.innerHTML = events.events[i].title;
+    
+        // create column for description
+        event_desc = document.createElement('div');
+        event_desc.classList.add('calendar-full-main-table-description'); 
+        event_desc.innerHTML = events.events[i].description;
+    
+        // create column for action buttons
+        let event_edit = document.createElement('button');
+        event_edit.classList.add('action_edit');
+        event_edit.innerHTML = `Bearbeiten`;
+        let event_del = document.createElement('button');
+        event_del.classList.add('action_del');
+        event_del.innerHTML = `Löschen`;
+        event_buttons = document.createElement('div');
+        event_buttons.classList.add('calendar-full-main-table-actions'); 
+        event_buttons.appendChild(event_edit);
+        event_buttons.appendChild(event_del);
+    
+        event_row.appendChild(event_date);
+        event_row.appendChild(event_time);
+        event_row.appendChild(event_title);
+        event_row.appendChild(event_desc);
+        event_row.appendChild(event_buttons);
+        event_content.appendChild(event_row);
+    
+        event_buttons.addEventListener('click', function(item) {
+            if(item.target == event_edit) {
+                let headline = 'Event Bearbeiten';
+                //removeModal();
+                showModal(headline, i, "edit");
+            } else if(item.target == event_del) {
+                let headline = 'Event Löschen';
+                //removeModal();
+                showModal(headline, i, "del");
+            } else {
+                console.log("Fehler: Dieser Button existiert nicht.");
+            }
+        });
+    }
+}
+down();
+function up() {
+    for(i=events_events_length-1;i>=0;i--) {
+        console.log(i);
+        let event_row = document.createElement('div');
+        event_row.classList.add('calendar-full-main-table-row');
+    
+        // create column for date
+        event_date = document.createElement('div');
+        event_date.classList.add('calendar-full-main-table-date'); 
+        event_date.innerHTML = events.events[i].date;
+    
+        // create column for time
+        event_time = document.createElement('div');
+        event_time.classList.add('calendar-full-main-table-time'); 
+        event_time.innerHTML = events.events[i].time;
+    
+        // create column for title
+        event_title = document.createElement('div');
+        event_title.classList.add('calendar-full-main-table-title'); 
+        event_title.innerHTML = events.events[i].title;
+    
+        // create column for description
+        event_desc = document.createElement('div');
+        event_desc.classList.add('calendar-full-main-table-description'); 
+        event_desc.innerHTML = events.events[i].description;
+    
+        // create column for action buttons
+        let event_edit = document.createElement('button');
+        event_edit.classList.add('action_edit');
+        event_edit.innerHTML = `Bearbeiten`;
+        let event_del = document.createElement('button');
+        event_del.classList.add('action_del');
+        event_del.innerHTML = `Löschen`;
+        event_buttons = document.createElement('div');
+        event_buttons.classList.add('calendar-full-main-table-actions'); 
+        event_buttons.appendChild(event_edit);
+        event_buttons.appendChild(event_del);
+    
+        event_row.appendChild(event_date);
+        event_row.appendChild(event_time);
+        event_row.appendChild(event_title);
+        event_row.appendChild(event_desc);
+        event_row.appendChild(event_buttons);
+        event_content.appendChild(event_row);
+    
+        event_buttons.addEventListener('click', function(item) {
+            if(item.target == event_edit) {
+                let headline = 'Event Bearbeiten';
+                //removeModal();
+                showModal(headline, i, "edit");
+            } else if(item.target == event_del) {
+                let headline = 'Event Löschen';
+                //removeModal();
+                showModal(headline, i, "del");
+            } else {
+                console.log("Fehler: Dieser Button existiert nicht.");
+            }
+        });
+    }
 }
 
 function showModal(headline, i, type) {
@@ -153,37 +217,57 @@ function showModal(headline, i, type) {
                                 <textarea>${events.events[i].description}</textarea>
                             </div>`;
                             
+    let modal_message_del = document.createElement('div');
+    modal_message_del.classList.add('modal-message');
+    modal_message_del.innerHTML = 
+                                `
+                                Möchtest du dieses Event wirklich löschen?<br><br>
+                                ${events.events[i].date}<br>
+                                ${events.events[i].title}
+                                `; 
 
     modal.style.display = 'flex';
 
     modal_container.appendChild(modal_headline);
     modal_container.appendChild(modal_content);
-    modal_container.appendChild(modal_actions);
 
-    if(type == 1) {
+
+    if(type == "edit") {
         modal_actions.appendChild(modal_action_safe);
         modal_actions.appendChild(modal_action_cancel);
-    } else if(type == 2) {
+    } else if(type == "del") {
         modal_actions.appendChild(modal_action_del);
         modal_actions.appendChild(modal_action_cancel);
+        modal_container.removeChild(modal_content);
+        modal_container.appendChild(modal_message_del);
     }
+
+    modal_container.appendChild(modal_actions);
 
     modal_action_cancel.addEventListener('click', function closeModal() {
         modal.style.display = 'none';
         modal_container.remove();
+        modal_message_del.remove();
         modal_action_del.remove();
         modal_action_safe.remove();
     });
 }
 
-// function removeModal() {
-//     modal_action_del.remove();
-//     modal_action_safe.remove();
-//     modal_container.remove();
-// }
-
-
-
 let spinner = document.querySelector('.button-refresh').addEventListener('click', () => {
     document.querySelector('.icon-refresh').classList.toggle('spin');
+});
+
+let sort = document.querySelector('.sort');
+sort.addEventListener('click', () => {
+    if(sort.classList.contains('up')) {
+        up();
+        sort.classList.remove('up');
+        sort.classList.add('down');
+    } else if(sort.classList.contains('down')) {
+        event_content.remove();
+        down();
+        sort.classList.remove('down');
+        sort.classList.add('up');
+    }
+
 });
